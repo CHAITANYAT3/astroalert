9253ec249bc1d4d725ef67dcfc80dd773a3094c1
CHAITANYAT3 <chaitanya.tankar@gmail.com>
2025-09-09 13:44:17 +0530

    Update build configuration for Vercel deployment

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,21 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  plugins: [
-    {
-      plugin: require("craco-cesium")(),
-    },
-  ],
   webpack: {
-    configure: (webpackConfig) => {
-      // Ensure html-webpack-plugin is before html-webpack-tags-plugin
-      const HtmlWebpackPlugin = webpackConfig.plugins.find(
-        (plugin) => plugin.constructor.name === "HtmlWebpackPlugin"
-      );
-      if (HtmlWebpackPlugin) {
-        webpackConfig.plugins = webpackConfig.plugins.filter(
-          (plugin) => plugin.constructor.name !== "HtmlWebpackTagsPlugin"
-        );
-      }
-      return webpackConfig;
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  },
+    plugins: {
+      add: [
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: 'node_modules/cesium/Build/Cesium',
+              to: 'cesium'
+            }
+          ]
+        }),
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify('/cesium')
+        })
+      ]
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        enforce: 'pre',
+        include: path.resolve(__dirname, 'node_modules/cesium'),
+        use: [{
+          loader: 'strip-pragma-loader',
+          options: {
+            pragmas: {
+              debug: false
+            }
+          }
+        }]
+      });
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        https: require.resolve('https-browserify'),
+        http: require.resolve('stream-http'),
+        url: require.resolve('url/'),
+        zlib: require.resolve('browserify-zlib'),
+        stream: require.resolve('stream-browserify'),
+      };
+      return webpackConfig;
+    }
+  }
 };
