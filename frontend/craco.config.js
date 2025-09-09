c40eaf7eae9c12bf7659fe82fae1d069e2e9b980
CHAITANYAT3 <chaitanya.tankar@gmail.com>
2025-09-09 13:40:39 +0530

    Fix webpack configuration for Vercel deployment

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -4,4 +4,18 @@ module.exports = {
       plugin: require("craco-cesium")(),
     },
   ],
+  webpack: {
+    configure: (webpackConfig) => {
+      // Ensure html-webpack-plugin is before html-webpack-tags-plugin
+      const HtmlWebpackPlugin = webpackConfig.plugins.find(
+        (plugin) => plugin.constructor.name === "HtmlWebpackPlugin"
+      );
+      if (HtmlWebpackPlugin) {
+        webpackConfig.plugins = webpackConfig.plugins.filter(
+          (plugin) => plugin.constructor.name !== "HtmlWebpackTagsPlugin"
+        );
+      }
+      return webpackConfig;
+    },
+  },
 };
